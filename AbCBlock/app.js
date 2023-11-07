1bce3864440444b146a0d6c1cdb224f9aecf89ec
Ayush_sccg <ayush1240945@gmail.com>
2023-11-07 15:50:35 +0545

    abcBlock

--- AbCBlock/app.js
+++ AbCBlock/app.js
@@ -1,5 +1,64 @@
-function answer() {
-    return 42;
+const blocks = [
+  ["B", "O"],
+  ["X", "K"],
+  ["D", "Q"],
+  ["C", "P"],
+  ["N", "A"],
+  ["G", "T"],
+  ["R", "E"],
+  ["T", "G"],
+  ["Q", "D"],
+  ["F", "S"],
+  ["J", "W"],
+  ["H", "U"],
+  ["V", "I"],
+  ["A", "N"],
+  ["O", "B"],
+  ["E", "R"],
+  ["F", "S"],
+  ["L", "Y"],
+  ["P", "C"],
+  ["Z", "M"],
+];
+
+const BOOOLEAN = [];
+// blocks.sort();
+
+for (let i = 0; i < 20; i++) {
+  BOOOLEAN.push(false);
+  // blocks[i].sort();
+}
+
+function checkChar(char) {
+  let str = "";
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 2; j++) {
+      if (blocks[i][j] === char && !BOOOLEAN[i]) {
+        BOOOLEAN[i] = true;
+        console.log(blocks[i][j]);
+        console.log(i);
+        str += blocks[i][j];
+        return str;
+      }
+    }
   }
-  
-  module.exports = answer;
+}
+
+function answer(word) {
+  let str = word;
+  let len = str.length;
+  let strArray = [];
+  for (let i = 0; i < len; i++) {
+    strArray.push(str[i]);
+  }
+  let temp = "";
+  for (let i = 0; i < len; i++) {
+    temp += checkChar(str[i]);
+  }
+  console.log(temp);
+
+  return temp == word
+}
+
+
+module.exports = answer;
--- AbCBlock/app.test.js
+++ AbCBlock/app.test.js
@@ -1,7 +1,47 @@
 const answer = require('./app');
 
+
+
 describe('answer', () => {
   it('to life the universe and everything', () => {
-    expect(answer()).toEqual(42);
+    expect(answer("A")).toEqual(true);
   });
 });
+
+describe('answer', () => {
+  it('to life the universe and everything', () => {
+    expect(answer("BARK")).toEqual(true);
+  });
+});
+
+describe('answer', () => {
+  it('to life the universe and everything', () => {
+    expect(answer("BOOK")).toEqual(false);
+  });
+});
+
+describe('answer', () => {
+  it('to life the universe and everything', () => {
+    expect(answer("TREAT")).toEqual(false);
+  });
+});
+
+describe('answer', () => {
+  it('to life the universe and everything', () => {
+    expect(answer("COMMON")).toEqual(false);
+  });
+});
+
+describe('answer', () => {
+  it('to life the universe and everything', () => {
+    expect(answer("SQUAD")).toEqual(false);
+  });
+});
+
+describe('answer', () => {
+  it('to life the universe and everything', () => {
+    expect(answer("CONFUSE")).toEqual(false);
+  });
+});
+
+
