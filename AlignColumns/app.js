6fe5df08fab789146c13763b7d92e20ae7eae4eb
Ayush_sccg <ayush1240945@gmail.com>
2023-11-08 12:34:14 +0545

    aligncolumns

--- AlignColumns/app.js
+++ AlignColumns/app.js
@@ -1,4 +1,20 @@
-function answer() {
-  return 42;
+function answer(sentence) {
+  let str = sentence;
+  let strArr = [];
+  for (let i in str){
+    strArr.push(str[i]);
+  }
+  for (let i in strArr){
+    if(strArr[i] === '$'){
+        strArr[i] = " ";
+    }
+  }
+  let ans = "";
+  for (let i in strArr){
+    ans += strArr[i];
+  }
+  return ans;
 }
+
+answer("A$C")
 module.exports = answer;
--- AlignColumns/app.test.js
+++ AlignColumns/app.test.js
@@ -1,7 +1,10 @@
 const answer = require("./app");
 
+const sentence = "Given$a$text$file$of$many$lines,$where$fields$within$a$line$are$delineated$by$a$single$'dollar'$character,$write$a$programthat$aligns$each$column$of$fields$by$ensuring$that$words$in$each$column$are$separated$by$at$least$one$space"
+const withSpace = "Given a text file of many lines, where fields within a line are delineated by a single 'dollar' character, write a programthat aligns each column of fields by ensuring that words in each column are separated by at least one space"
+
 describe("answer", () => {
   it("to life the universe and everything", () => {
-    expect(answer()).toEqual(42);
+    expect(answer(sentence)).toEqual(withSpace);
   });
 });
