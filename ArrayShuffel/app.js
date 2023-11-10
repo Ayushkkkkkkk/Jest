acf8bf6a3ce49b3de31298fbca7db9ed93476fdd
Ayush_sccg <ayush1240945@gmail.com>
2023-11-10 11:44:28 +0545

    .

--- ArrayShuffel/app.js
+++ ArrayShuffel/app.js
@@ -1,5 +1,30 @@
-function answer() {
-    return 42;
+function swap(a, b, arr) {
+  let temp;
+  temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+
+function answer(lower_bound, upper_bound) {
+  let arr = [];
+  for (let i = lower_bound; i < upper_bound; i++) {
+    arr.push(i);
   }
-  console.log("working");
-  module.exports = answer;
+
+  let rand = Math.floor(Math.random() * upper_bound - 1 + lower_bound);
+  let firstPtr = 0;
+  let secondPtr = rand;
+  while (firstPtr < arr.length && secondPtr < arr.length) {
+    if(secondPtr >= arr.length){
+        break;
+    }
+    swap(firstPtr , secondPtr , arr);
+    firstPtr++;
+    secondPtr++;
+  }
+  console.log(rand);
+  console.log(arr);
+}
+answer(1, 10);
+
+module.exports = answer;
--- ArrayShuffel/app.test.js
+++ ArrayShuffel/app.test.js
@@ -5,3 +5,11 @@ describe('answer', () => {
     expect(answer()).toEqual(42);
   });
 });
+
+
+describe('answer', () => {
+    it('to life the universe and everything', () => {
+      expect(answer(1 , 7)).toEqual(42);
+    });
+  });
+  
